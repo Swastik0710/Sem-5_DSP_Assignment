64a7c0e01e341e83410b0b7e0234e5e140740997
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-07-29 06:41:45 +0530

    Create parabolic_signal.m

---
+++ Basic Signals/Continuous Domain/parabolic_signal.m
@@ -0,0 +1,12 @@
+% PARABOLIC SIGNAL (=t^2 when t≥0; =0 when t<0)
+t=-5:0.1:5;
+A=2;
+x1=(A*(t.^2))/2;
+x2=0;
+x=x1.*(t>=0)+x2.*(t<0);
+subplot(3,3,5);
+stem(t,x,'r');
+xlabel('Time');
+ylabel('Amplitude');
+title('Parabolic Signal');
+grid on;
