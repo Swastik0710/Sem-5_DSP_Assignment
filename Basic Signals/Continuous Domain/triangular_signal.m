b534fbe2aaa8af615c6593b853d9b6290c118c36
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-08-02 06:38:09 +0530

    Create triangular_signal.m

---
+++ Basic Signals/Continuous Domain/triangular_signal.m
@@ -0,0 +1,12 @@
+% TRIANGULAR SIGNAL (=1-|t|/a when |t|≤a; =0 when |t|>a)
+t=-5:0.1:5;
+a=2;
+x1=1-abs(t)/a;
+x2=0;
+x=x1.*(abs(t)<=a)+x2.*(abs(t)>a);
+subplot(3,3,9);
+stem(t,x,'g');
+xlabel('Time');
+ylabel('Amplitude');
+title('Triangular Signal');
+grid on;
