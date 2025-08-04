e8d028d9cf8d23a259a30af6bf789ec48546cf8c
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-08-04 11:17:53 +0530

    Create unit_step_signal.m

---
+++ Basic Signals/Discrete Domain/unit_step_signal.m
@@ -0,0 +1,11 @@
+% UNIT STEP SIGNAL (=1 when t>0; =0 when t<0)
+t=-5:0.1:5;
+x1=1;
+x2=0;
+x=x1.*(t>=0)+x2.*(t<0);
+subplot(3,3,2);
+stem(t,x,'g');
+xlabel('Time');
+ylabel('Amplitude');
+title('Unit Step Signal');
+grid on;
