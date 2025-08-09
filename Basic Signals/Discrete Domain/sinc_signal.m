df0196d9c68c1f986cea3ff9fcb315ea3e471bc6
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-08-09 06:47:40 +0530

    Create sinc_signal.m

---
+++ Basic Signals/Discrete Domain/sinc_signal.m
@@ -0,0 +1,11 @@
+% SINC SIGNAL (=1 when t=0; =sin(t)/t when t≠0)
+t=-5:0.1:5;
+x1=1;
+x2=(sin(t)./t);
+x=x1.*(t==0)+x2.*(t~=0);
+subplot(3,3,7);
+stem(t,x,'r');
+xlabel('Time');
+ylabel('Amplitude');
+title('Sinc Signal');
+grid on;
