e0264b41203bc075975d1a53d6491125215d6c28
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-12-21 12:10:29 +0530

    Create circular_convolution.m

---
+++ Circular Convolution/Continuous Domain/circular_convolution.m
@@ -0,0 +1,41 @@
+clc;
+clear all;
+close all;
+X=input('Enter the sequence of x(n): ');
+H=input('Enter the sequence of h(n): ');
+M=length(X);
+L=length(H);
+N=max(M,L);
+x=[X zeros(N-M)];
+h=[H zeros(N-L)];
+y=zeros(1,N);
+for i=0:N-1
+  for j=0:N-1
+    k=mod((i-j),N);
+    y(i+1)=y(i+1)+x(j+1).*h(k+1);
+  end
+end
+disp('Circularly convolved sequence: ');
+disp(y);
+n=0:N-1;
+subplot(3,1,1);
+plot(n,x,'r');
+title('Input Sequence x(n)');
+xlabel('Discrete Time (n)--->');
+ylabel('Amplitude of x(n)--->');
+grid on;
+hold on;
+subplot(3,1,2);
+plot(n,h,'g');
+title('Impulse Response h(n)');
+xlabel('Discrete Time (n)--->');
+ylabel('Amplitude of h(n)--->');
+grid on;
+hold on;
+subplot(3,1,3);
+plot(n,y,'b');
+title('Circularly Convolved Sequence y(n)');
+xlabel('Discrete Time (n)--->');
+ylabel('Amplitude of y(n)--->');
+grid on;
+hold on;
